7382002b5a9efdb0a20beb497645e73cd367778a
Nancyb <nancyeb27@yahoo.com>
2019-02-24 15:14:08 -0700

    Updates

---
+++ precache-manifest.61c689a7ee086bda71fe90233ba5e29f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b39f0cd0fe0befe20dc4",
+    "url": "/clicky-app/static/js/runtime~main.b39f0cd0.js"
+  },
+  {
+    "revision": "45ad4dad65ecbf9d49d0",
+    "url": "/clicky-app/static/js/main.45ad4dad.chunk.js"
+  },
+  {
+    "revision": "77356571d154ee60d5ee",
+    "url": "/clicky-app/static/js/2.77356571.chunk.js"
+  },
+  {
+    "revision": "45ad4dad65ecbf9d49d0",
+    "url": "/clicky-app/static/css/main.35046ae6.chunk.css"
+  },
+  {
+    "revision": "77356571d154ee60d5ee",
+    "url": "/clicky-app/static/css/2.a5d58761.chunk.css"
+  },
+  {
+    "revision": "2edb47d3bc5b439c2440ce623de37d75",
+    "url": "/clicky-app/index.html"
+  }
+];
